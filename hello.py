c1faf0837278d993c00abb14040b1fc3ad20ef19
Mansiha verma <mnishuverma444@gmail.com>
2020-08-30 07:31:37 +0530

    first commit by mv

---
+++ hello.py
@@ -0,0 +1,29 @@
+a=14
+print(type(a))
+
+b=True
+print(type(b))
+
+
+x=33
+c=int(x)
+
+print(hex(10))
+
+print(oct(10))
+
+s="hello"
+print(s)
+
+s1="""you are awsome
+hello hiii
+you are cute"""
+print(s1)
+
+print(s[2])
+
+print(s*3 
+print(len(s1)
+
+printf("len(3)")
+
